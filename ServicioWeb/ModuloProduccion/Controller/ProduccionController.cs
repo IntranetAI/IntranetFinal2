fe49692c4c7d054aee194d04725e3caea60ae2de
cjerias <carlosjerias@gmail.com>
2018-01-22 17:58:39 -0300

    Solucion problema correo distribucion x ot

--- ServicioWeb/ModuloProduccion/Controller/ProduccionController.cs
+++ ServicioWeb/ModuloProduccion/Controller/ProduccionController.cs
@@ -4342,9 +4342,7 @@ namespace ServicioWeb.ModuloProduccion.Controller
                 foreach (string NumeroOT in lista.Select(o => o.OT).Distinct())
                 {
                     System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
-                    int count = 0;
-                    string TabladtDistribuccuion = "";
-                    string FechasDistribuccion = "";
+                    int count = 0; string Contenido = "";string Correos = "";string InsertFechaDistribucion = "";
                     foreach (FechaDistribuccion dt in lista.Where(o => o.OT == NumeroOT))
                     {
                         if (count == 0)
@@ -4352,7 +4350,11 @@ namespace ServicioWeb.ModuloProduccion.Controller
                             string[] splitCorreo = dt.Correo.Substring(0, dt.Correo.Length - 1).Split(';');
                             foreach (string correouser in splitCorreo)
                             {
-                                mmsg.To.Add(correouser);
+                                if (!Correos.Contains(correouser.ToLower()))
+                                {
+                                    mmsg.To.Add(correouser);
+                                    Correos += correouser;
+                                }
                             }
                             mmsg.To.Add("correofechadistribucionxot@aimpresores.cl");
                             //mmsg.To.Add("carlos.jerias.r@aimpresores.cl");
@@ -4367,7 +4369,7 @@ namespace ServicioWeb.ModuloProduccion.Controller
                             mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
                             count++;
                         }
-                        TabladtDistribuccuion += "<tr style='height: 22px; background: rgb(255, 255, 255); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: normal; font-stretch: normal; font-size: 11px; line-height: normal; font-family: arial, helvetica, sans-serif; color: rgb(51, 51, 51); vertical-align: text-top;'>" +
+                        Contenido += "<tr style='height: 22px; background: rgb(255, 255, 255); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: normal; font-stretch: normal; font-size: 11px; line-height: normal; font-family: arial, helvetica, sans-serif; color: rgb(51, 51, 51); vertical-align: text-top;'>" +
                                                         "<td style='font-weight: normal; padding: 4px 0px 5px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: right; width: 50px;'>" + dt.OT + "</td>" +
                                                         "<td style='font-weight: normal; padding: 4px 0px 5px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: left; width: 143px;'>" + dt.NombreOT + "</td>" +
                                                         "<td style='font-weight: normal; padding: 4px 0px 5px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: left; width: 123px;'>" + dt.Cliente + "</td>" +
@@ -4382,185 +4384,16 @@ namespace ServicioWeb.ModuloProduccion.Controller
                                                  "</tr>";
                         string[] split1 = dt.FechaPrevista.Split(' ');
                         string[] splitFecha = split1[0].Split('-');
-                        FechasDistribuccion += "insert into Produccion_FechaDistribuccion( NumOrdem,NombreOT,Cliente,Fecha_Prevista,Cantidad,TipodeReparto,Destinatario,Direccion, " +
+                        InsertFechaDistribucion += "insert into Produccion_FechaDistribuccion( NumOrdem,NombreOT,Cliente,Fecha_Prevista,Cantidad,TipodeReparto,Destinatario,Direccion, " +
                                                 "Observacion,MedioTransporte,Ciudad,Pais) " +
                                                 "values('" + dt.OT + "','" + dt.NombreOT + "','" + dt.Cliente + "','" + splitFecha[2] + "-" + splitFecha[1] + "-" + splitFecha[0] + " " + split1[1] + "','" + dt.Cantidad + "','" + dt.TipoReparto + "','" + dt.Destinatario +
                                                 "','" + dt.Direccion + "','" + dt.Observacion + "','" + dt.MedioTransporte + "','" + dt.Ciudad + "','" + dt.Pais + "')";
                     }
-                    mmsg.Body = "<html lang='en'><head>" +
-
-        "<!--[if mso]>" +
-            "<style>" +
-                " * {" +
-                    " font-family: sans-serif !important;" +
-                "}" +
-            "</style>" +
-        "<![endif]-->" +
-
-        "<!-- All other clients get the webfont reference; some will render the font and others will silently fail to the fallbacks. More on that here: http://stylecampaign.com/blog/2015/02/webfont-support-in-email/ -->" +
-        "<!--[if !mso]><!-->" +
-            "<!-- insert web font reference, eg: <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'> -->" +
-        "<!--<![endif]-->" +
-
-        "<!-- Web Font / @font-face : END -->" +
-
-        "<!-- CSS Reset -->" +
-        "<style>" +
-
-            " html," +
-            " body {" +
-                " margin: 0 auto !important;" +
-                " padding: 0 !important;" +
-                " height: 100% !important;" +
-                " width: 100% !important;" +
-            " }" +
-
-            " /* What it does: Stops email clients resizing small text. */" +
-            " * {" +
-                " -ms-text-size-adjust: 100%;" +
-                " -webkit-text-size-adjust: 100%;" +
-            " }" +
-
-            " /* What it does: Centers email on Android 4.4 */" +
-            " div[style*='margin: 16px 0'] {" +
-                " margin:0 !important;" +
-            " }" +
-
-            " /* What it does: Stops Outlook from adding extra spacing to tables. */" +
-            " table," +
-            " td {" +
-                " mso-table-lspace: 0pt !important;" +
-                " mso-table-rspace: 0pt !important;" +
-            " }" +
-
-            " /* What it does: Fixes webkit padding issue. Fix for Yahoo mail table alignment bug. Applies table-layout to the first 2 tables then removes for anything nested deeper. */" +
-            " table {" +
-                " border-spacing: 0 !important;" +
-                " border-collapse: collapse !important;" +
-                " table-layout: fixed !important;" +
-                " margin: 0 auto !important;" +
-            " }" +
-            " table table table {" +
-                " table-layout: auto;" +
-            " }" +
-
-            " /* What it does: Uses a better rendering method when resizing images in IE. */" +
-            " img {" +
-                " -ms-interpolation-mode:bicubic;" +
-            " }" +
-
-            " /* What it does: A work-around for iOS meddling in triggered links. */" +
-            " *[x-apple-data-detectors] {" +
-                " color: inherit !important;" +
-                " text-decoration: none !important;" +
-            " }" +
-
-            " /* What it does: A work-around for Gmail meddling in triggered links. */" +
-            " .x-gmail-data-detectors," +
-            " .x-gmail-data-detectors *," +
-            " .aBn {" +
-                " border-bottom: 0 !important;" +
-                " cursor: default !important;" +
-            " }" +
-
-            " /* What it does: Prevents Gmail from displaying an download button on large, non-linked images. */" +
-            " .a6S {" +
-                " display: none !important;" +
-                " opacity: 0.01 !important;" +
-            " }" +
-            " /* If the above doesn't work, add a .g-img class to any image in question. */" +
-            " img.g-img + div {" +
-                " display:none !important;" +
-            " }" +
-
-            " /* What it does: Prevents underlining the button text in Windows 10 */" +
-            " .button-link {" +
-                " text-decoration: none !important;" +
-            " }" +
-
-            " @media only screen and (min-device-width: 375px) and (max-device-width: 413px) { /* iPhone 6 and 6+ */" +
-                " .email-container {" +
-                    " min-width: 375px !important;" +
-                " }" +
-            " }" +
-
-        " </style>" +
-
-        " <!-- What it does: Makes background images in 72ppi Outlook render at correct size. -->" +
-        " <!--[if gte mso 9]>" +
-        " <xml>" +
-            " <o:OfficeDocumentSettings>" +
-                " <o:AllowPNG/>" +
-                " <o:PixelsPerInch>96</o:PixelsPerInch>" +
-            " </o:OfficeDocumentSettings>" +
-        " </xml>" +
-        " <![endif]-->" +
-
-        " <!-- Progressive Enhancements -->" +
-        " <style>" +
-
-            " /* What it does: Hover styles for buttons */" +
-            " .button-td," +
-            " .button-a {" +
-                " transition: all 100ms ease-in;" +
-            " }" +
-            " .button-td:hover," +
-            " .button-a:hover {" +
-                " background: #555555 !important;" +
-                " border-color: #555555 !important;" +
-            " }" +
-
-        " </style>" +
-
-    " </head>" +
-    " <body width='100%' style='margin: 0; mso-line-height-rule: exactly;'>" +
-        " <center style='width: 100%; text-align: left;'>" +
-
-            " <!-- Visually Hidden Preheader Text : BEGIN -->" +
-            " <div style='display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;'>" +
-                " Estimado(a) : Este informe entrega las fechas de Distribucción." +
-            " </div>" +
-            " <!-- Visually Hidden Preheader Text : END -->" +
-
-            " <!--" +
-                " Set the email width. Defined in two places:" +
-                " 1. max-width for all clients except Desktop Windows Outlook, allowing the email to squish on narrow but never go wider than 1200px." +
-                " 2. MSO tags for Desktop Windows Outlook enforce a 1200px width." +
-            " -->" +
-            " <div style='max-width: 1200px; margin: auto;' class='email-container'>" +
-                " <!--[if mso]>" +
-                " <table role='presentation' aria-hidden='true' cellspacing='0' cellpadding='0' border='0' width='1200' align='center'>" +
-                " <tr>" +
-                " <td>" +
-                " <![endif]-->" +
-
-                " <!-- Email Header : BEGIN -->" +
-                 "<table role='presentation' aria-hidden='true' cellspacing='0' cellpadding='0' border='0' align='center' width='100%' style='max-width: 1200px;'> " +
-                     "<tbody><tr> " +
-                         "<td style='padding: 0 20px 0 20px; text-align: center'> " +
-                             "<img src='http://intranet.qgchile.cl/images/Logo color lateral.jpg' aria-hidden='true' width='276' height='50' alt='alt_text' border='0' style='height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;'> " +
-                         " </td> " +
-                     "</tr> " +
-                 "</tbody></table> " +
-                " <!-- Email Header : END --> " +
-
-                 "<!-- Email Body : BEGIN --> " +
-                " <table role='presentation' aria-hidden='true' cellspacing='0' cellpadding='0' border='0' align='center' width='100%' style='max-width: 1200px;'> " +
-                     "<!-- 1 Column Text + Button : BEGIN --> " +
-                     "<tr> " +
-                         "<td bgcolor='#ffffff'> " +
-                             "<table role='presentation' aria-hidden='true' cellspacing='0' cellpadding='0' border='0' width='100%'> " +
-                                 "<tbody><tr> " +
-                                     "<td style='padding: 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;'> " +
-                                         "Estimado(a): <br /><br />" +
-                                         "Este informe se obtiene de forma automática desde el control de Fecha Distribucción (Metrics Jobtrack)." +
-                                         "<br><br> " +
-
-                                     "</td> " +
-                                     "</tr> " +
-
-                     "<tr><td>  " +
-                        "<table id='tblRegistro' cellspacing='0' cellpadding='0' style='border: 1px solid rgb(204, 204, 204); margin: 0px auto 15px 3px; width: 100%;'><tbody>" +
+                    mmsg.Body = "<img src='http://intranet.qgchile.cl/Images/LOGO%20A.png' width='267px'  height='67px' />" +
+                            "<br/><br/>Estimado(a):" +
+                            "<br/><br/>Este informe se obtiene de forma automática desde el control de Fecha Distribucción (Metrics Jobtrack)." +
+                            "<br/><br/>" +
+                            "<table id='tblRegistro' cellspacing='0' cellpadding='0' style='border: 1px solid rgb(204, 204, 204); margin: 0px auto 15px 3px; width: 100%;'><tbody>" +
                             "<tr style='height: 22px; background: rgb(243, 244, 249); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: normal; font-stretch: normal; font-size: 11px; line-height: normal; font-family: arial, helvetica, sans-serif; color: rgb(0, 62, 126); text-align: left;'> " +
                                 "<td style='font-weight: bold; padding: 4px 0px 0px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: center; width: 50px;'>OT</td> " +
                                 "<td style='font-weight: bold; padding: 4px 0px 0px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: center; width: 143px;'>Nombre OT</td>" +
@@ -4573,73 +4406,31 @@ namespace ServicioWeb.ModuloProduccion.Controller
                                 "<td style='font-weight: bold; padding: 4px 0px 0px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: center; width: 103px;'>Dirección</td>" +
                                 "<td style='font-weight: bold; padding: 4px 0px 0px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: center; width: 73px;'>Ciudad/Pais</td>" +
                                 "<td style='font-weight: bold; padding: 4px 0px 0px 5px; border-right: 1px solid rgb(204, 204, 204); text-align: center; width: 183px;'>Observación</td>" +
-
-
-
-
-
-
-                            "</tr>" +
-                            TabladtDistribuccuion +
-                    "</tbody></table> " +
-                     "</td></tr>" +
-                             "</tbody></table> " +
-                         "</td> " +
-                     "</tr> " +
-
-
-                " </tbody></table>" +
-                " <!-- Email Body : END -->" +
-
-                " <!-- Email Footer : BEGIN -->" +
-                " <table role='presentation' aria-hidden='true' cellspacing='0' cellpadding='0' border='0' align='center' width='100%' style='max-width: 680px;'>" +
-                    " <tbody><tr>" +
-                        " <td style='padding: 40px 10px;width: 100%;font-size: 12px; font-family: sans-serif; line-height:18px; text-align: center; color: #888888;' class='x-gmail-data-detectors'>" +
-                            " <webversion style='color:#cccccc; text-decoration:underline; font-weight: bold;'>Atentamente</webversion>" +
-                            " <br><br> " +
-                            " Equipo de desarrollo A Impresores S.A. " +
-                            " <br><br>" +
-                        " </td>" +
-                    " </tr>" +
-                " </tbody></table> " +
-                " <!-- Email Footer : END --> " +
-
-                " <!--[if mso]>" +
-                " </td> " +
-                " </tr> " +
-                " </table> " +
-                " <![endif]--> " +
-            " </div> " +
-        " </center> " +
-    " </body></html>";
+                                "</tr>" +
+                                Contenido.ToString() + "</table>" +
+                            "<br />" +
+                             "Atentamente," +
+                            "<br />" +
+                            "<b>Equipo de desarrollo A Impresores S.A.</b>";
                     mmsg.BodyEncoding = System.Text.Encoding.UTF8;
                     mmsg.IsBodyHtml = true; //Si no queremos que se envíe como HTML
-
                     mmsg.From = new System.Net.Mail.MailAddress("sistema.intranet@aimpresores.cl");
-
-
-
                     System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
-                    cliente.Credentials =
-                        new System.Net.NetworkCredential("sistema.intranet@aimpresores.cl", "SI2013.");
-
-
+                    cliente.Credentials = new System.Net.NetworkCredential("sistema.intranet@aimpresores.cl", "SI2013.");
                     cliente.Host = "mail.aimpresores.cl";
-
                     try
                     {
                         cliente.Send(mmsg);
-                        QueryDtDistribuccion += FechasDistribuccion;
-                        FechasDistribuccion = "";
+                        QueryDtDistribuccion += InsertFechaDistribucion;
                     }
                     catch (System.Net.Mail.SmtpException ex)
                     {
-                        FechasDistribuccion = "";
                         string NombreProcedure0 = ex.StackTrace.ToString().Substring(ex.StackTrace.ToString().IndexOf("ProduccionController.") + 21, ex.StackTrace.Length - (ex.StackTrace.ToString().IndexOf("ProduccionController.") + 21));
                         string NombreProcedure = NombreProcedure0.Substring(0, NombreProcedure0.IndexOf("("));
                         GenerarCorreoErrordeEnvio("FechaEntregaEnviodeCorreoAutomatico", "Especifico", NombreProcedure, DateTime.Now.ToString("dd-MM-yyyy"), ex.Message);
                         return "Error Enviado";
                     }
+                    count = 0;Correos = "";
                 }
             }
             else
